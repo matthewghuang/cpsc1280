1b9b9eda1cc5c1f14ca321a74bdc604e2221aa57
matthewghuang <matthewhuang604@gmail.com>
2023-05-21 21:01:00 -0700

    finish assignment2

--- assignment2/script2_1a.sh
+++ assignment2/script2_1a.sh
@@ -9,29 +9,35 @@ mkdir -p question1/RED_VELVET/Queendom/Pose
 mkdir -p question1/TXT
 mkdir -p question1/Kep1er
 
->question1/BTS/file1.txt
->question1/BTS/file2.txt
->question1/BTS/file3.txt
->question1/BTS/COLD_PLAY/Universe/mars.txt
->question1/BTS/COLD_PLAY/Universe/venus.txt
->question1/BTS/COLD_PLAY/Universe/Jupiter.txt
->question1/BTS/Idol/fileA.txt
->question1/BTS/Idol/fileB.txt
->question1/BTS/Idol/fileC.txt
->question1/BTS/Dna/fileA.txt
->question1/BTS/Dna/fileB.txt
->question1/BTS/Dna/fileC.txt
->question1/BTS/Butter/milk.txt
->question1/BTS/Butter/fileC.txt
->question1/BTS/Butter/oil.txt
->question1/BTS/Dynamite/coolshades.txt
->question1/BTS/Dynamite/KingKong.txt
->question1/RED_VELVET/Rythm/hello.txt
->question1/RED_VELVET/Psycho/hello.txt
->question1/RED_VELVET/Queendom/Pose/strike.txt
->question1/TXT/song.txt
->question1/Kep1er/test.txt
+cp reference.txt question1/BTS/file1.txt
+cp reference.txt question1/BTS/file2.txt
+cp reference.txt question1/BTS/file3.txt
+cp reference.txt question1/BTS/COLD_PLAY/Universe/mars.txt
+cp reference.txt question1/BTS/COLD_PLAY/Universe/venus.txt
+cp reference.txt question1/BTS/COLD_PLAY/Universe/Jupiter.txt
+cp reference.txt question1/BTS/Idol/fileA.txt
+cp reference.txt question1/BTS/Idol/fileB.txt
+cp reference.txt question1/BTS/Idol/fileC.txt
+cp reference.txt question1/BTS/Dna/fileA.txt
+cp reference.txt question1/BTS/Dna/fileB.txt
+cp reference.txt question1/BTS/Dna/fileC.txt
+cp reference.txt question1/BTS/Butter/fileC.txt
+cp reference.txt question1/BTS/Dynamite/KingKong.txt
+cp reference.txt question1/RED_VELVET/Queendom/Pose/strike.txt
+cp reference.txt question1/TXT/song.txt
+cp reference.txt question1/Kep1er/test.txt
 
+# make links
+ln question1/BTS/Dna/fileA.txt question1/BTS/Dynamite/coolshades.txt 
+ln question1/BTS/Dna/fileB.txt question1/RED_VELVET/Rythm/hello.txt  
+ln question1/BTS/Dna/fileC.txt question1/RED_VELVET/Psycho/hello.txt  
+ln question1/BTS/Dna/fileA.txt question1/BTS/Butter/oil.txt  
+ln question1/BTS/COLD_PLAY/Universe/Jupiter.txt question1/BTS/Butter/milk.txt  
+
+chmod ug=rwx,o=rx question1/BTS
+chmod u=rwx,g-rwx,o=rx question1/Kep1er
+chmod ug=rwx,o=rx question1/RED_VELVET
+chmod ug=rwx,o=rx question1/TXT
 chmod ug=rw,o=r question1/BTS/*
 chmod a=rx question1/BTS/COLD_PLAY
 chmod ug=rwx,o=rx question1/BTS/COLD_PLAY/Universe
