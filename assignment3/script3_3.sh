585586dda55d6d1e34048116e0924dbf82861e45
matthewghuang <matthewhhuang@gmail.com>
2023-05-24 13:59:13 -0700

    fix script3_4

--- assignment3/script3_3.sh
+++ assignment3/script3_3.sh
@@ -1,1 +1,3 @@
-echo "$1" is a member of the twoset '$(violin)', he has perfect pitch. "$1" has been playing the \'violin\' for 10\* years\[ref 123\], and he\'s known to have \"Perfect Pitch\",\{ref 345\}. "$1" has played in the Australian Orechestra\*\.
+set -f -u
+
+echo "$1" is a member of twoset '$(violin)', he has perfect pitch. "$1" has been playing the \'violin\' for 10* years[ref 123], and he\'s known to have \"Perfect Pitch\",{ref 345}. "$1" has played in Australian Orechestra*.
