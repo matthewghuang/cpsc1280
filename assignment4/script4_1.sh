110ca163eb5e731a4ced3c4a80037fa97eece4f9
matthewghuang <matthewhuang604@gmail.com>
2023-06-04 23:29:38 -0700

    update to use ls instead of find

--- assignment4/script4_1.sh
+++ assignment4/script4_1.sh
@@ -1,10 +1,11 @@
 dirname="$1"
 
-# echo "owner,group,filename,permissions"
-user=$(echo owner; find "$dirname" -maxdepth 1 -type f -printf "%u")
-group=$(echo group; find "$dirname" -maxdepth 1 -type f -printf "%g")
-name=$(echo filename; find "$dirname" -maxdepth 1 -type f -printf "%p")
-perms=$(echo permissions; find "$dirname" -maxdepth 1 -type f -printf "%M")
+echo "owner,group,filename,permissions"
+# user=$(echo owner; find "$dirname" -maxdepth 1 -type f -printf "%u")
+user=$(ls -l "$dirname" | tail -n +2 | cut -d " " -f 3)
+group=$(ls -l "$dirname" | tail -n +2 | cut -d " " -f 4)
+name=$(printf "$dirname/"; ls -l "$dirname" | tail -n +2 | cut -d " " -f 10)
+perms=$(ls -l "$dirname" | tail -n +2 | cut -d " " -f 1)
 
 paste_output=$(paste -d"," <(echo "$user") <(echo "$group") <(echo "$name") <(echo "$perms"))
 sort <(echo "$paste_output") -t "," -k 3
